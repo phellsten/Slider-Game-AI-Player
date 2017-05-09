30e457160c2fceaaf4793aedd42a8ab2e81bcb6c
phellsten <phellsten@student.unimelb.edu.au>
2017-05-09 17:45:04 +1000

    Implemented update

--- src/aiproj/slider/SliderPhai.java
+++ src/aiproj/slider/SliderPhai.java
@@ -9,6 +9,7 @@ import aiproj.slider.Move;
 public class SliderPhai implements SliderPlayer {
 
 	Board board;
+	char player;
 	
 	public SliderPhai() {
 		
@@ -17,6 +18,7 @@ public class SliderPhai implements SliderPlayer {
 	@Override
 	public void init(int dimension, String board, char player) {
 		this.board = new Board(board, dimension);
+		this.player = player;
 		System.out.println("0, 0: " + this.board.blocks[0][0]);
 		System.out.println("1, 2: " + this.board.blocks[1][2]);
 		System.out.println("3, 1: " + this.board.blocks[3][1]);
@@ -24,8 +26,27 @@ public class SliderPhai implements SliderPlayer {
 
 	@Override
 	public void update(Move move) {
-		// TODO Auto-generated method stub
-		
+		// current board representation : board;
+		// opponents move: move;
+		// opponent moved piece board[move.i][move.j] in direction move.d
+		// new board representation:...
+		int x = move.i;
+		int y = move.j;
+		String piece = board.blocks[x][y];
+		board.blocks[x][y] = " ";
+		if(move.d == Move.Direction.UP) {
+			y++;
+		}
+		else if(move.d == Move.Direction.RIGHT) {
+			x++;
+		}
+		else if(move.d == Move.Direction.DOWN) {
+			y--;
+		}
+		else if(move.d == Move.Direction.LEFT) {
+			x--;
+		}
+		board.blocks[x][y] = piece;
 	}
 
 	@Override
