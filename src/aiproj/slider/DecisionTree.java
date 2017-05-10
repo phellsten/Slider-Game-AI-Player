76d6c98909617b625e420e6d2ac091c7370a0575
Patrick <pat.hudgell@yahoo.com.au>
2017-05-10 21:00:01 +1000

    Add decision tree Node internal class

--- src/aiproj/slider/DecisionTree.java
+++ src/aiproj/slider/DecisionTree.java
@@ -1,7 +1,37 @@
 package aiproj.slider;
 
 public class DecisionTree {
+	private class decisionNode {
+		// Value calculated by MiniMax
+		int value;
+		// The move that results in this value
+		Move move;
+	}
 
 	// We can afford 3 ply toilet paper, unlike the University
 	public static final int PLY_LENGTH = 3;
+	
+	// Board the game is to be played on
+	private Board board;
+
+	DecisionTree(Board board) {
+		// Start construction of DecisionTree, and its root nodes
+		this.board = board;
+		
+		// Start calculation of possible moves.
+		
+	}
+	
+	private void calculateMoves(Board board)
+	{
+		
+	}
+	
+	/**
+	 * Moves the DecisionTree to the appropriate node, and recalcuate bottom
+	 * values
+	 */
+	public void move(Move move) {
+
+	}
 }
