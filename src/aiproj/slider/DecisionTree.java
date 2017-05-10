c97cf9608ca6ba483e4f6d9f3e3a1f4bfa187590
Patrick <pat.hudgell@yahoo.com.au>
2017-05-10 22:26:17 +1000

    Add in move function from previous assigment

--- src/aiproj/slider/DecisionTree.java
+++ src/aiproj/slider/DecisionTree.java
@@ -4,9 +4,11 @@ import java.util.ArrayList;
 
 public class DecisionTree {
 	private class decisionNode {
-
 		// Value calculated by MiniMax
 		private int value;
+		
+		// The children of the node
+		ArrayList<decisionNode> childNodes;
 
 		// The move that results in this value
 		private Move move;
@@ -26,10 +28,8 @@ public class DecisionTree {
 		public void setMove(Move move) {
 			this.move = move;
 		}
-
-		ArrayList <decisionNode> childNodes;
 	}
-	
+
 	// We can afford 3 ply toilet paper, unlike the University
 	public static final int PLY_LENGTH = 3;
 
@@ -38,21 +38,54 @@ public class DecisionTree {
 
 	// The root node of the decision tree
 	private decisionNode rootNode;
+	
+	// The string of the player
+	String playerString;
 
-	DecisionTree(Board board) {
+	DecisionTree(Board board, String playerString) {
 		// Start construction of DecisionTree, and its root nodes
 		this.board = board;
-
+		this.playerString = playerString;
 		// Start calculation of possible moves.
-		calculateMoves(board, rootNode);
+		calculateMoves(board, rootNode, this.playerString);
 	}
+
+	/** Creates a new simulated move */
 
 	/**
 	 * Calculates the moves for the board, and places them in the speicified
 	 * node
 	 */
-	private void calculateMoves(Board board, decisionNode node) {
+	private void calculateMoves(Board board, decisionNode node, String player) {
 
+		int i;
+		int j;
+		int numLegalH = 0;
+		int numLegalV = 0;
+
+		// For each piece on the board
+		for (j = 0; j < board.size; j++) {
+			for (i = 0; i < board.size; i++) {
+				// If piece is 'H'
+				if (board.blocks[i][j].equals(player)) {
+					if (board.isFree(i + 1, j, "H")) {
+						// Create a new node with the move
+						
+						
+						// H can move right
+						numLegalH++;
+					}
+					if (board.isFree(i, j - 1, player)) {
+						// H can move up
+						numLegalH++;
+					}
+					if (board.isFree(i, j + 1, player)) {
+						// H can move down
+						numLegalH++;
+					}
+				}
+			}
+		}
 	}
 
 	/**
