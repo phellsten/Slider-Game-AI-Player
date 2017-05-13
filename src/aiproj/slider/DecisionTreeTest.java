e933e449628305094c18d306b7abc78c4a7cd1dc
Patrick <pat.hudgell@yahoo.com.au>
2017-05-13 14:46:22 +1000

    Test Decision tree moving

--- src/aiproj/slider/DecisionTreeTest.java
+++ src/aiproj/slider/DecisionTreeTest.java
@@ -4,12 +4,12 @@ public class DecisionTreeTest {
 	public static void main(String[] args) {
 		System.out.println("TEST");
 		// Create a normal 3x3 board
-		// Board testBoard = new Board("H + + + \nH + + + \nH + + +\n+ V V V\n", 4);
-		Board testBoard = new Board("+ + + H \n+ + + + \n+ + + +\n+ + + +\n", 4);
+		Board testBoard = new Board("H + + + \nH + + + \nH + + +\n+ V V V\n", 4);
+		//Board testBoard = new Board("+ + + H \n+ + + + \n+ + + +\n+ + + +\n", 4);
 		// Make the Decision Tree Great Again
 		DecisionTree testTree = new DecisionTree(testBoard, "H");
 		// Test the moves
 		testTree.calculatePossibleMoves("H");
-		testTree.move(new Move(3,3,Move.Direction.DOWN));
+		testTree.move(new Move(0,1,Move.Direction.RIGHT));
 	}
 }
